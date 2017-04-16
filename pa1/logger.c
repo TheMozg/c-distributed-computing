f61eea850e93b46bc94910ee10df5c2428025e80
Yakkodash <al.milos@mail.ru>
2017-04-17 01:26:07 +0300

    Modify logger

--- pa1/logger.c
+++ pa1/logger.c
@@ -2,37 +2,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdarg.h>
 
 #include "logger.h"
 #include "pa1.h"
 
-int log_error ( const char *str ) {
+void log_error ( const char *str ) {
     write( STDERR_FILENO, str, sizeof(str) / sizeof(char) );
     write( STDERR_FILENO, "\n", 1 );
-    return -1;
 }
 
-int log_output ( const char *format, const char *str ) {
-    printf( format, str );
-    return 0;
+void log_output ( const char *format, ... ) {
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
 }
 
-int log_started ( local_id id, pid_t pid, pid_t parent ) {
-    printf( log_started_fmt, id, pid, parent );
-    return 0;
+void log_started ( local_id id, pid_t pid, pid_t parent ) {
+    log_output( log_started_fmt, id, pid, parent );
 }
 
-int log_done ( local_id id ) {
-    printf( log_done_fmt, id );
-    return 0;
+void log_done ( local_id id ) {
+    log_output( log_done_fmt, id );
 }
 
-int log_received_all_started ( local_id id ) {
-    printf( log_received_all_started_fmt, id );
-    return 0;
+void log_received_all_started ( local_id id ) {
+    log_output( log_received_all_started_fmt, id );
 }
 
-int log_received_all_done ( local_id id ) {
-    printf( log_received_all_done_fmt, id );
-    return 0;
+void log_received_all_done ( local_id id ) {
+    log_output( log_received_all_done_fmt, id );
+}
+
+void log_open_pipe ( int fd ) {
+
 }
--- pa1/logger.h
+++ pa1/logger.h
@@ -6,16 +6,16 @@
 
 #include "ipc.h"
 
-int log_error ( const char *str );
+void log_error ( const char *str );
 
-int log_output ( const char *format, const char *str );
+void log_output ( const char *format, ... );
 
-int log_started ( local_id id, pid_t pid, pid_t parent );
+void log_started ( local_id id, pid_t pid, pid_t parent );
 
-int log_done ( local_id id );
+void log_done ( local_id id );
 
-int log_received_all_started ( local_id id );
+void log_received_all_started ( local_id id );
 
-int log_received_all_done ( local_id id );
+void log_received_all_done ( local_id id );
 
 #endif //_LOGER_H_
