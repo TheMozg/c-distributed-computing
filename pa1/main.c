01174f56485b931e477ec3fd37123a7ed924a4f4
Fedor Kalugin <fedor.md@gmail.com>
2017-04-16 15:11:55 +0000

    Add crude IPC using pipe

--- pa1/main.c
+++ pa1/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <string.h>
 #include "pa1.h"
 
 static char doc[] = "ITMO Distributed Computing programming assignment #1";
@@ -31,6 +32,14 @@ int main (int argc, char **argv) {
     int process_count = 0;
     argp_parse (&argp, argc, argv, 0, 0, &process_count);
 
+    int fd_read[1024];
+    int fd_writ[1024];
+    for (size_t i = 0; i <= process_count; i++) {
+        int fd[2];
+        pipe(fd);
+        fd_read[i] = fd[0];
+        fd_writ[i] = fd[1];
+    }
     int id = 0;
     for (size_t i = 1; i <= process_count; i++) {
         if (id == 0) {
@@ -38,12 +47,33 @@ int main (int argc, char **argv) {
             if (pid == 0) {
                 id = i;
                 printf(log_started_fmt, (int)id, getpid(), getppid());
-                sleep(5);
             }
         }
     }
 
+    close(fd_writ[id]);
+    for (size_t i = 0; i <= process_count; i++) {
+        if (i != id) {
+            close(fd_read[i]);
+        }
+    }
+    char    string[] = "P_\n";
+    string[1] = id;
+    char    readbuffer[80];
+    int nbytes;
+    for (size_t i = 0; i <= process_count; i++) {
+        if (i == id) {
+            continue;
+        }
+        write(fd_writ[i], string, (strlen(string)+1));
+        printf("P %d sent to: %d\n", id,(int)i);
+    }
+    for (size_t i = 0; i < process_count; i++) {
+        nbytes = read(fd_read[id], readbuffer, strlen(string)+1);
+        int id_r = readbuffer[1];
+        printf("P %d received from: %d\n", id,id_r);
+    }
     while( wait(NULL) > 0 );
-
+    printf("P %d quit\n", id);
     exit (0);
 }
