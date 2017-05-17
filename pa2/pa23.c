c1980be7f02bbfb154813a990f084f866d43c896
Fedor Kalugin <fedor.md@gmail.com>
2017-05-17 22:50:13 +0300

    Add argument parsing

--- pa2/pa23.c
+++ pa2/pa23.c
@@ -1,3 +1,9 @@
+#include <stdlib.h>
+#include <argp.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
 #include "banking.h"
 
 void transfer(void * parent_data, local_id src, local_id dst,
@@ -6,10 +12,63 @@ void transfer(void * parent_data, local_id src, local_id dst,
     // student, please implement me
 }
 
+static char doc[] = "ITMO Distributed Computing programming assignment #2";
+
+static struct argp_option options[] = {
+    {0, 'p', "PROCESS_COUNT", 0, "Number of child processes" },
+    {0}
+};
+
+typedef struct {
+  int balance[MAX_PROCESS_ID];
+  int process_count;
+} args_t;
+
+static error_t parse_opt (int key, char *arg, struct argp_state *state) {
+    args_t *args = state->input;
+    switch (key)
+    {
+    case 'p':
+        args->process_count = atoi(arg);
+        if (args->process_count < 0 || args->process_count > MAX_PROCESS_ID){
+            argp_failure(state, 1, 0,"PROCESS_COUNT must be between 0 and %d",MAX_PROCESS_ID);
+        }
+        break;
+
+    case ARGP_KEY_ARG:
+        if (state->arg_num >= args->process_count){
+            argp_usage (state);
+        }
+        args->balance[state->arg_num] = atoi(arg);
+        break;
+
+    case ARGP_KEY_END:
+        if (state->arg_num < args->process_count){
+            argp_usage (state);
+        }
+        break;
+
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = { options, parse_opt, 0, doc };
+
 int main(int argc, char * argv[])
 {
     //bank_robbery(parent_data);
     //print_history(all);
+
+    args_t args;
+    argp_parse (&argp, argc, argv, 0, 0, &args);
+    local_id process_count = args.process_count;
+    balance_t balance[process_count];
+    for (local_id i = 0; i < args.process_count; i++){
+        balance[i] = args.balance[i];
+        printf (i == 0 ? "%d" : ", %d", balance[i]);
+    }
 
     return 0;
 }
