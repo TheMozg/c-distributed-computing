b875c11753687281efc36661807b30cad67f6b3f
Yakkodash <al.milos@mail.ru>
2017-05-22 23:40:03 +0300

    Fixed IPC reads

--- pa2/ipc.c
+++ pa2/ipc.c
@@ -12,9 +12,13 @@
 
 #ifdef _DEBUG_IPC_
     #define FAIL_SLEEP 10000
+    #define DEBUG(x) x
+#else
+    #define DEBUG(x) __asm__("nop")
 #endif
+
 /*
- * O_NONBLOCK enabled, n <= PIPE_BUF (n in theory is always less 
+ * O_NONBLOCK enabled, n <= PIPE_BUF (n in theory is always less
  * than PIPE_BUF because PIPE_BUF is 16 pages, so our msg struct
  * should fit).
  * If there is room to write n bytes to the pipe, then write(2)
@@ -24,14 +28,20 @@
 
 int send(void * self, local_id dst, const Message * msg) {
     proc_t * proc = self;
-    int status = write(proc->fd_writ[proc->id][dst], msg, sizeof(MessageHeader) + msg->s_header.s_payload_len);
-    
+    int status = write(proc->fd_writ[proc->id][dst], (char *)msg, sizeof(MessageHeader) +
+                                msg->s_header.s_payload_len);
+
     #ifdef _DEBUG_IPC_
     printf("Sending from %d to %d\n", proc->id, dst);
     if (status == -1) {
         printf("Failed to write, id %d: %s\n", proc->id, strerror(errno));
         usleep(FAIL_SLEEP);
         return -1;
+    }
+    if (status < sizeof(MessageHeader) + msg->s_header.s_payload_len) {
+
+    printf("DIDNT WRITE ENOUGH BYTES, id %d\n", proc->id);
+    return -1;
     }
     #endif
 
@@ -53,26 +63,33 @@ int send_multicast(void * self, const Message * msg) {
 int receive(void * self, local_id from, Message * msg) {
     proc_t * proc = self;
 
-    int status = read(proc->fd_read[proc->id][from], msg, sizeof(MessageHeader));
-    if (status != sizeof(MessageHeader) && status != -1 && status != 0){
-        fprintf(stderr, "Critial error: partial receive (message header) - expected %lu bytes, got %d.\n", sizeof(MessageHeader), status);
-    }
-    if (status == sizeof(MessageHeader) && msg->s_header.s_payload_len != 0){
-        int payload_status = read(proc->fd_read[proc->id][from], msg->s_payload, msg->s_header.s_payload_len);
-        if (payload_status != msg->s_header.s_payload_len){
-            fprintf(stderr, "Critial error: partial receive (message payload) - expected %hu bytes, got %d.\n", msg->s_header.s_payload_len, status);
+    // Checks for errors from read. Mostly to avoid EBADF.
+    while( read(proc->fd_read[proc->id][from], NULL, 0) == -1 );
+
+   /*
+    * EAGAIN
+    * The file descriptor fd refers to a file other 
+    * than a socket and has been marked nonblocking 
+    * (O_NONBLOCK), and the read would block.
+    */
+
+    while ( read(proc->fd_read[proc->id][from], &(msg->s_header), sizeof(MessageHeader)) == -1 ) {
+       if (errno != EAGAIN) {
+            DEBUG(fprintf(stderr, "Crit err: partial receive (message header) - expected %lu bytes, got %d.\n",
+                sizeof(MessageHeader), status));
+            return -1;
         }
     }
 
-    #ifdef _DEBUG_IPC_
-    if (status == -1) {
-        printf("Failed to read from %d, id %d: %s\n", from, proc->id, strerror(errno));
-        usleep(FAIL_SLEEP);
+    int payload_len = msg->s_header.s_payload_len;
+    while (read(proc->fd_read[proc->id][from], msg->s_payload, payload_len) != payload_len) {
+
+        DEBUG(fprintf(stderr, "Crit err: partial receive (message payload) - expected %hu bytes, got %d.\n",
+            msg->s_header.s_payload_len, status));
         return -1;
     }
-    #endif
 
-    return (status == -1 || status == 0) ? -1 : 0;
+    return 0;
 }
 
 /*
@@ -80,13 +97,18 @@ int receive(void * self, local_id from, Message * msg) {
  * Do not receive messages from process itself.
  */
 int receive_any(void * self, Message * msg) {
-    proc_t * proc = self;
-    while(1){
+    proc_t *proc = self;
+    while (1) {
         for ( local_id from = 0; from < proc->process_count; from++ ) {
             if ( from != proc->id ) {
-                int status = receive( self, from, msg );
-                if ( status != -1 ) return 0;
+                if ( read( proc->fd_read[proc->id][from], &(msg->s_header), sizeof(Message) ) > 0 ) {
+                    if ( msg->s_header.s_payload_len > 0 ) {
+                        read( proc->fd_read[proc->id][from], (char *)&(msg->s_payload), 
+                                msg->s_header.s_payload_len );
+                    }
+                    return 0;
+                }
             }
-        } 
+        }
     }
 }
